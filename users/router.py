b2fa4e4be4c57dd0668e3760a3a2d719d5e32245
justAnotherRepoCreater <manishwalurkar10@gmail.com>
2025-09-03 12:09:46 +0530

    warehouse model added

---
+++ users/router.py
@@ -0,0 +1,7 @@
+# Scope creation endpoint will not be made as we only have to manually add 3-4 roles into the table
+
+
+# Company related endpoints
+# Company and admin user's creation endpoint
+# Company updation endpoint (scope: admin only)
+# company deletion endpoint (scope: admin only, related dependency will be deleted)
--- users/schema.py
+++
@@ -1,3 +0,0 @@
-class Constants:
-    live = 'live'
-    deleted = 'deleted'
