86d55752d2629b1f6df0fe7271a1756befddb4b2
tejastathedev <tejas.tathe.dev@gmail.com>
2025-09-03 19:01:59 +0530

    exceptional handler

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
 from database import Base, engine
 from utils.exception_handler import ExceptionHandler
 app = FastAPI()
 
 @app.get('/')
 def home_function():
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Data not found")
     return "Server is up !"
 
 # call exception handler for raising all the exception from one file/location
